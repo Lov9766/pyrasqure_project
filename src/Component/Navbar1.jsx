2512e5d8ef054d5faff3002659522756596c533c
Lov9766 <sharmalk9766@gmail.com>
2022-04-20 00:29:05 +0530

    react app

---
+++ src/Component/Navbar1.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+
+const Container = styled.div`
+  height: 60px;
+  /* background-color:black;  */
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+align-items: center;
+  display: flex;
+  justify-content:space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display:flex;
+align-items: center;
+margin-left:25px;
+padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo=styled.h1`
+    
+`
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+font-size: 14px;
+margin-left: 25px;
+cursor: pointer;
+
+
+`;
+
+
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <SearchIcon style={{color:"grey",fontSize:"16px" }}/>
+          </SearchContainer>
+        </Left>
+
+        <Center>
+          <Logo>PYRA JWELS</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGNIN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+             <ShoppingCartOutlinedIcon/>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Component/Navbar1'
+
+const Home = () => {
+
+
+  return (
+      <div>
+          
+          <Navbar/>
+    </div>
+  )
+}
+
+export default Home
